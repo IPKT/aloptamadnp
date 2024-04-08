8c481c1d3f9fca3434fd2ab1fb6ed4687d4cb66a
IPKT <putu.kembar@gmail.com>
2024-04-08 15:38:36 +0800

    edit con

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 // */
-$config['base_url'] = 'http://182.23.56.142:90/aloptamadnp/';
+$config['base_url'] = 'http://localhost/aloptamadnp/';
 
 /*
 |--------------------------------------------------------------------------
